cd8b8f0e04dc4b79e5baa308ba8d8dd80829f44b
orianaRomera <orianaromera22@gmail.com>
2024-10-06 16:08:47 +0000

    punto f

--- Parcial1/puntof.sh
+++ Parcial1/puntof.sh
@@ -1,6 +1,6 @@
 cat > Filtro_Avanzado.txt
 
-echo "Mi IP publida es: $(curl -s ifconfig.me)" >> Filtro_Avanzado.txt
+echo "Mi IP publica es: $(curl -s ifconfig.me)" >> Filtro_Avanzado.txt
 echo "Mi usuario es: $(whoami)" >> Filtro_Avanzado.txt
 echo "El Hash de mi usuario es: $(sudo cat /etc/shadow |grep vagrant |awk -F ':' '{print $2}')" >> Filtro_Avanzado.txt
 
